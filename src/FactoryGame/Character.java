f1154ae17b58132d7f4064faf39951ed98946ea2
= <philipp.gatzka@gmx.ch>
2021-01-15 12:01:51 +0100

    Emergency Save so i dont lose my Progress in case of failure wen adding Pathfinding

--- src/FactoryGame/Character.java
+++ src/FactoryGame/Character.java
@@ -1,9 +1,21 @@
 package FactoryGame;
 
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
-
-	public Character() {
-		// TODO Auto-generated constructor stub
+	private CharacterSpritesheet sheet;
+	public Character(CharacterSpritesheet sheet) {
+		this.sheet = sheet;
 	}
-
+	
+	public void render(Graphics2D g) {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
 }
--- src/FactoryGame/CharacterGenerator.java
+++ src/FactoryGame/CharacterGenerator.java
@@ -2,10 +2,12 @@ package FactoryGame;
 
 import java.io.IOException;
 
-public class CharacterGenerator {
+public class CharacterGenerator { 
 	public CharacterGenerator() {
 		
-			CharacterSpritesheet human = new CharacterSpritesheet("human.png");
-		
+	}
+	
+	public void createCharacter() {
+		Character character = new Character(new CharacterSpritesheet("humand.png"));
 	}
 }
